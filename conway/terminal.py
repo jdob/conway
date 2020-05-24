61e8c0ee06af334f067d0480490237904adb0746
jdob <jdobies@redhat.com>
2020-05-24 16:21:34 -0400

    Added ability to center curses output

--- conway/terminal.py
+++ conway/terminal.py
@@ -9,21 +9,27 @@ class TerminalView(object):
         super().__init__()
         self.gol = gol
         self.screen = None
+        self.x_offset = 0
+        self.y_offset = 0
 
     def run(self):
         self.screen = curses.initscr()
         self.screen.clear()
         self.screen.refresh()
 
+        # Color setup
         curses.start_color()
         curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
+        # General curses settings
         curses.curs_set(0)
+
+        self._calculate_offsets()
 
         try:
             self._run_game()
-        except KeyboardInterrupt:
-            print('Exiting')
+        except:
+            pass
 
         curses.curs_set(1)
         curses.endwin()
@@ -41,6 +47,14 @@ class TerminalView(object):
     def _display_glyph(self, x, y):
         z = self.gol.grid[x, y]
         if z == game.ON:
-            self.screen.addstr(x, y, 'o', curses.color_pair(1))
+            self.screen.addstr(y + self.y_offset, x + self.x_offset,
+                               'o', curses.color_pair(1) | curses.A_BOLD)
         else:
-            self.screen.addstr(x, y, ' ')
+            self.screen.addstr(y + self.y_offset, x + self.x_offset,
+                               ' ')
+
+    def _calculate_offsets(self):
+        num_rows, num_cols = self.screen.getmaxyx()
+
+        self.x_offset = int((num_cols - len(self.gol.grid)) / 2) - 1
+        self.y_offset = int((num_rows - len(self.gol.grid)) / 2) - 1
